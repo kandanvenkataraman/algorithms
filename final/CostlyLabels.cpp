91ac6a6c07a425deef9464d82f6573d9c1b421a9
Kandan <kandan.venkataraman@gmail.com>
2016-03-08 05:15:14 +1100

    white space changes

--- final/CostlyLabels.cpp
+++ final/CostlyLabels.cpp
@@ -22,129 +22,129 @@ int hungarian(int N)
  
 	// we seek to minimize the number of point we give away
 	for (int i = 0; i < N; i++) {
-	   int m = INT_MAX;
-	   for (int j = 0; j < N; j++)
-	      m = min(m, M[i][j]);
-	   C += m;
-	   for (int j = 0; j < N; j++)
-	      M[i][j] -= m;
+		int m = INT_MAX;
+		for (int j = 0; j < N; j++)
+			m = min(m, M[i][j]);
+		C += m;
+		for (int j = 0; j < N; j++)
+			M[i][j] -= m;
 	}
  
 	for (int i = 0; i < N; i++) {
-	   int m = INT_MAX;
-	   for (int j = 0; j < N; j++)
-	      m = min(m, M[j][i]);
-	   C += m;
-	   for (int j = 0; j < N; j++)
-	      M[j][i] -= m;
+		int m = INT_MAX;
+		for (int j = 0; j < N; j++)
+			m = min(m, M[j][i]);
+		C += m;
+		for (int j = 0; j < N; j++)
+			M[j][i] -= m;
 	}
  
 	int MX[30];
 	int MY[30];
  
 	for (int i = 0; i < N; i++)
-	   MX[i] = MY[i] = -1;
+		MX[i] = MY[i] = -1;
  
 	int matchCount = 0;
  
 	while (matchCount < N) {
  
-	   queue<int> QS;
+		queue<int> QS;
  
 		bool S[30];
 		bool T[30];
-	   int PX[30];
-	   int PY[30];
+		int PX[30];
+		int PY[30];
  
-	   for (int i = 0; i < N; i++) {
+		for (int i = 0; i < N; i++) {
 			S[i] = T[i] = false;
-	      PX[i] = PY[i] = -1;
-	      if (MX[i] == -1) {
-	         QS.push(i);
-	      }
-	   }
+			PX[i] = PY[i] = -1;
+			if (MX[i] == -1) {
+				QS.push(i);
+			}
+		}
  
-	   int freeY = -1;
+		int freeY = -1;
  
 buildtree:
-	   while (!QS.empty()) {
+		while (!QS.empty()) {
 
-	      int x = QS.front();
-	      QS.pop();
+			int x = QS.front();
+			QS.pop();
  
 			if (S[x])
-	         continue;
+				continue;
 			
 			S[x] = true;
  
-	      for (int i = 0; i < N; i++) {
+			for (int i = 0; i < N; i++) {
 				if (M[x][i] != 0 || T[i])
 					continue;
 				T[i] = true;
-	         PY[i] = x;
-	         if (MY[i] == -1) {
-	            freeY = i;
-	            break;
-	         }
-	         else {
-	            PX[MY[i]] = i;
-	            QS.push(MY[i]);
-	         }
-	      }
-	      if (freeY != -1)
-	         break;
-	   }
+				PY[i] = x;
+				if (MY[i] == -1) {
+					freeY = i;
+					break;
+				}
+				else {
+					PX[MY[i]] = i;
+					QS.push(MY[i]);
+				}
+			}
+			if (freeY != -1)
+				break;
+		}
 
 incrmatchset:
-	   if (freeY != -1) {
-	   	int count = 0;
-	   	int curr = freeY;
-	      // augment match path to matches;
+		if (freeY != -1) {
+			int count = 0;
+			int curr = freeY;
+			// augment match path to matches;
  
-	      while (curr != -1) {
+			while (curr != -1) {
  
-	         int prev = (count %2 == 0 ? PY[curr] : PX[curr]);
-	         if (prev == -1)
-	            break;
+				int prev = (count %2 == 0 ? PY[curr] : PX[curr]);
+				if (prev == -1)
+					break;
  
-	         if (count %2 == 0) {
-	            MY[curr] = prev;
-	            MX[prev] = curr;
-	         }
+				if (count %2 == 0) {
+					MY[curr] = prev;
+					MX[prev] = curr;
+				}
  
-	         curr = prev;
-	         count++;
-	      }
-	      matchCount++;
-	      continue;
-	   }
+				curr = prev;
+				count++;
+			}
+			matchCount++;
+			continue;
+		}
 
   		int theta = INT_MAX;
 
-	   for (int i = 0; i < N; i++)  {
-	      for (int j = 0; j < N; j++) {
-	         if (S[i] && !T[j])
-	            theta = min(theta, M[i][j]);
-	      }
-	   }
+		for (int i = 0; i < N; i++)  {
+			for (int j = 0; j < N; j++) {
+				if (S[i] && !T[j])
+					theta = min(theta, M[i][j]);
+			}
+		}
  
 		queue<int> QT;
-	   for (int i = 0; i < N; i++)  {
+		for (int i = 0; i < N; i++)  {
 			if (S[i])
 				C += theta;
 			if (T[i])
 				C -= theta;
-	      for (int j = 0; j < N; j++) {
+			for (int j = 0; j < N; j++) {
 				if (S[i])
-	            M[i][j] -= theta;
+					M[i][j] -= theta;
 				if (T[j])
-	            M[i][j] += theta;
+					M[i][j] += theta;
 				else if (S[i] && M[i][j] == 0 && PY[j] == -1) {
 					QT.push(j);
 					PY[j] = i;
 				}
-	      }
-	   }
+			}
+		}
  
 		while (!QT.empty()) {
 			int x = QT.front();
@@ -275,11 +275,11 @@ int main()
 			}
 		}
 
-	   int m = INT_MAX;
+		int m = INT_MAX;
 		for (int j = 0; j < K; j++)
 			m = min(m, Calc(tree, root, j, K));
 
-	   cout << "Case #" << i+1 << ": " << m << endl;
+		cout << "Case #" << i+1 << ": " << m << endl;
 	}
  
 	return 0;
